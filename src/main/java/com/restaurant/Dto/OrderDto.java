25361280a07d5b79252ef6b835d7f2eeaf0fcb8f
DanielPopielski <danielek6699@wp.pl>
2024-03-19 16:20:20 +0100

    added OrderDto

---
+++ src/main/java/com/restaurant/Dto/OrderDto.java
@@ -0,0 +1,17 @@
+package com.restaurant.Dto;
+
+import com.restaurant.Entity.OrderItemEntity;
+import com.restaurant.Entity.TableEntity;
+import com.restaurant.Entity.UserEntity;
+import com.restaurant.Enum.OrderStatus;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * A DTO for the {@link com.restaurant.Entity.OrderEntity} entity
+ */
+@Data
+public record OrderDto(UserEntity user, TableEntity table, List<OrderItemEntity> orderItems, OrderStatus status) implements Serializable {
+}
